b0b13d8946ba0ed1b0a1e177a3e067b24872d63c
Aashray Gupta <127435012+AashrayGithub@users.noreply.github.com>
2024-06-04 23:33:56 +0530

    Update NFTpage.js

--- src/components/NFTpage.js
+++ src/components/NFTpage.js
@@ -131,16 +131,16 @@ export default function NFTPage(props) {
       <div className="flex ml-20 mt-20">
         <img src={data.image} alt="" className="w-2/5" />
         <div className="text-xl ml-20 space-y-8 text-white shadow-2xl rounded-lg border-2 p-5">
-          <div>Name : {data.name}</div>
-          <div>Description : {data.description}</div>
+          <div>Name: {data.name}</div>
+          <div>Description: {data.description}</div>
           <div>
-            Price : <span className="">{data.price + " ETH"}</span>
+            Price: <span className="">{data.price + " ETH"}</span>
           </div>
           <div>
-            Owner : <span className="text-sm">{data.owner}</span>
+            Owner: <span className="text-sm">{data.owner}</span>
           </div>
           <div>
-            Seller : <span className="text-sm">{data.seller}</span>
+            Seller: <span className="text-sm">{data.seller}</span>
           </div>
           <div>
             {
